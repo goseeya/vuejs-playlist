3946227423d22cf4ca35ec4fab2000168ab4b3e5
goseeya <149542@gmail.com>
2018-05-03 09:37:43 +0200

    retrieving posts from firebase

--- src/components/listBlogs.vue
+++ src/components/listBlogs.vue
@@ -22,7 +22,7 @@ export default {
     
   },
   created() {
-      this.$http.get('https://jsonplaceholder.typicode.com/posts/').then(function(data){
+      this.$http.get('https://vuejs-blog-4b7b1.firebaseio.com/post.json').then(function(data){
           this.blogs = data.body.slice(0,10);
       })
   },
