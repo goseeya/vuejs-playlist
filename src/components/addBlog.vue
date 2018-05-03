0e6707b3130324e59466b85e312e6be21d47201d
goseeya <149542@gmail.com>
2018-05-03 08:45:45 +0200

    posting to firebase

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -57,11 +57,7 @@ export default {
   },
   methods: {
     post: function(){
-        this.$http.post('http://jsonplaceholder.typicode.com/posts', {
-            title: this.blog.title,
-            body: this.blog.content,
-            userId: 1
-        }).then(function(data){
+        this.$http.post('https://vuejs-blog-4b7b1.firebaseio.com/post.json', this.blog).then(function(data){
             console.log(data);
             this.submitted = true;
         });
